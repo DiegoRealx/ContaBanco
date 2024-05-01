13ea79119042e9a293bd4b3c0fc6ed887d848547
DiegoRealx <realdiego697@gmail.com>
2024-05-01 16:12:12 -0300

    projeto conta banco

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class ContaBanco {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int conta = 1021;
+        String agencia = "067-8";
+        String nome = "Mario Jorge";
+        double saldo = 237.48;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Bem-Vindo a Banco Dio");
+        System.out.println("Digite o  numero da sua conta: ");
+        int numeroConta = scanner.nextInt();
+            if(numeroConta == conta){
+                System.err.println("Ola " + nome + " é um prazer ter o senhor como cliente do nosso banco ");
+                System.out.println("sua agencia é: " + agencia + " e o seu salto atual é: "+ saldo +" ."  );
+            }
+            else{
+                System.err.println("Numero não encotrado; ");
+            }
+        }
     }
-}
